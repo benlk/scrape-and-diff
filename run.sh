700a901b80c44a60635b430d6fad4e05ef6e5ed8
Ben Keith <ben@benlk.com>
2015-08-28 12:57:01 -0400

    Put site screenshots in site folders

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ -f site-list.txt ] && [ `stat --format=%Y nokogiri.rb` -le `stat --format=%Y site-list.txt` ]
-then
-	echo "continuing"
-    continue
-else
-    ruby nokogiri.rb > site-list.txt
-fi
+#if [ -f site-list.txt ] && [ `stat --format=%Y nokogiri.rb` -le `stat --format=%Y site-list.txt` ]
+#then
+#	echo "continuing"
+#    continue
+#else
+#    ruby nokogiri.rb > site-list.txt
+#fi
 
 for URL in $(cut -d, -f2 < site-list.txt)
 do
--- screenshot.js
+++ screenshot.js
@@ -4,7 +4,8 @@ var args = system.args;
 var webpage = require('webpage')
 
 function rendermax(site) {
-  var slug = site.replace('http://','').replace(/\//g,'_');
+  var slug = site.split('/')[2];
+  var filename = site.replace('http://','').replace(/\//g,'_');
   var page = webpage.create();
   page.viewportSize= {
     width: 1600,
@@ -13,17 +14,17 @@ function rendermax(site) {
 
   console.log(site);
   page.open(site, function(status) {
-    console.log('\topened ' + slug);
+    console.log('\topened ' + filename);
     console.log('\t' + status);
     if (status !== 'success') {
-      console.error('Unable to load '+slug+'!');
+      console.error('Unable to load '+filename+'!');
       page.close();
       phantom.exit();
     } else {
-      console.log('\tattempting to render ' + slug);
+      console.log('\tattempting to render ' + filename);
       window.setTimeout(function () {
-        page.render('img/'+slug+'.png', {format: 'png'});
-        console.log('\trendered img/' + slug + '.png');
+        page.render('img/'+ slug + '/' +filename+'.png', {format: 'png'});
+        console.log('\trendered img/' + slug + '/'  + filename + '.png');
         page.close();
         phantom.exit();
       }, 1000); // Change timeout as required to allow sufficient time
